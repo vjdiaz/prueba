a9ee91b60fc98564fdbeb327b2b55b837bf4f9eb
practica <practica@pc-14-141.etsii>
2017-09-04 12:27:31 +0100

    Gracias por la invitación

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("¡Hola, Víctor!");
+		System.out.println("¡Hola, Víctor! Gracias por invitarme");
 	}
 
 }
